4bc5762991fd26215195387d4aef3fa26296c35e
Tobias Stenby Brixen <Tobias.Brixen@cryptomathic.com>
2018-06-22 07:39:49 +0200

    Update windows installer

--- install.bat
+++ install.bat
@@ -1,1 +1,6 @@
+@echo [+] Creating symlink to %~dp0.vimrc from %HOMEPATH%\_vimrc
 mklink %HOMEPATH%\_vimrc %~dp0.vimrc
+@echo [+] Please open vim and run :PluginInstall
+@echo [+] Please put the following into your .gitconfig
+@echo [include]
+@echo     path = %~dp0.gitconfig
