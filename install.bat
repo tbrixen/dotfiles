076b802257701bb575db0272eb61b0956beade2e
Tobias Stenby Brixen <Tobias.Brixen@cryptomathic.com>
2019-03-04 08:37:59 +0100

    Update windows installer

--- install.bat
+++ install.bat
@@ -1,6 +1,13 @@
-@echo [+] Creating symlink to %~dp0.vimrc from %HOMEPATH%\_vimrc
-mklink %HOMEPATH%\_vimrc %~dp0.vimrc
-@echo [+] Please open vim and run :PluginInstall
-@echo [+] Please put the following into your .gitconfig
-@echo [include]
-@echo     path = %~dp0.gitconfig
+@echo [+] Creating symlink to %~dp0vimrc from %HOMEPATH%\_vimrc
+@mklink %HOMEPATH%\_vimrc %~dp0vimrc
+
+@echo [+] Creating symlink to %~dp0vimfile\ from %HOMEPATH%\.vim\
+@mklink /D %HOMEPATH%\vimfiles %~dp0vim
+
+@echo [+] Creating symlink to %~dp0gitconfig from %HOMEPATH%\.gitconfig
+@mklink %HOMEPATH%\.gitconfig %~dp0gitconfig
+
+@echo [+] Please run the following to download dotbot and vim plugins
+@echo   git submodule update --init --recursive
+
+pause
