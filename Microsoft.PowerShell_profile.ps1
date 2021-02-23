4b847327b3d80f01b680f096335f5bbfb078aff9
tbrixen <tbrixen@gmail.com>
2021-02-23 21:53:22 +0100

    Add tab-completion for ssh config and known_hosts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -88,3 +88,50 @@ function ggco
 function Vimwiki-Goto { vim -c "VimwikiIndex" -c "VimwikiGoto $args" }
 Set-Alias -Name ww -Value Vimwiki-Goto
 
+# Tab-completion for ~/.ssh/config and ~/.ssh/known_hosts
+# Stolen from https://github.com/michaeloyer/windows-setup/blob/master/Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
+function Get-TabCompleteContext($wordToComplete, $commandAst) {
+	if ($commandAst -notmatch ' ') {
+		return ''
+	}
+
+	$subCommands = ($commandAst -split ' ', 2)[1].Trim() -replace '\s+', ' ' `
+		-replace '(\B|\s)\-.*(?=\s\-)', ""
+
+	$context = ($subCommands -replace ([Regex]::Escape($wordToComplete) + "$"), '').Trim() `
+		-replace '\s+' + [Regex]::Escape($wordToComplete) + "$", ''
+
+	return $context 
+}
+
+Register-ArgumentCompleter -Native -CommandName 'ssh' -ScriptBlock {
+	param($wordToComplete, $commandAst, $cursorPosition)
+
+	$context = Get-TabCompleteContext $wordToComplete $commandAst
+	
+	switch ($context) {
+		'-i' {
+			Get-ChildItem ~/.ssh | 
+			Select -ExpandProperty FullName |
+			ForEach-Object { "$wordToComplete $_".TrimStart() }
+		}
+		'' {
+			$ssh_hosts = 
+			Get-Content ~/.ssh/config -ErrorAction Ignore |
+			Select-String ^Host -Raw |
+			ForEach-Object { $_ -split '\s+' | Where-Object { -not [string]::IsNullOrEmpty($_) } | Select-Object -Skip 1 } |
+			Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
+			
+			$known_hosts =
+			Get-Content ~/.ssh/known_hosts -ErrorAction Ignore |
+			ForEach-Object { $_ -split '\s' | Select-Object -First 1 } |
+			ForEach-Object { $_ -split ',' } |
+			ForEach-Object { $_ -replace '^\[' -replace '\]:\d+$' } |
+			Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
+			
+			($ssh_hosts + $known_hosts) |
+			Select-Object -Unique |
+			Where-Object { $_ -like "$wordToComplete*" }
+		}
+	}
+}
