f3bb81c680162c08aeb4f6666f81a09f327a2462
Tobias Brixen <tbrixen@gmail.com>
2023-07-07 08:34:55 +0200

    Add config of shfmt

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -2,8 +2,14 @@ return {
   {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
+      local null_ls = require("null-ls")
+
+      opts.sources = {
+        null_ls.builtins.formatting.shfmt.with({
+          args = { "-i", "4", "-sr" }, -- https://github.com/mvdan/sh/blob/master/cmd/shfmt/shfmt.1.scd
+        }),
+      }
       if type(opts.sources) == "table" then
-        local null_ls = require("null-ls")
         vim.list_extend(opts.sources, {
           null_ls.builtins.diagnostics.tfsec,
         })
