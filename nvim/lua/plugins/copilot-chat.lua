e68ed7ce447bcb83831587560802d0af121e0f5e
Tobias Stenby Brixen <kind.job1347@fastmail.com>
2024-05-24 20:46:03 +0200

    Add copilot-prompts

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -14,11 +14,26 @@ return {
         Review = "Review the following code and provide concise suggestions.",
         Tests = "Briefly explain how the selected code works, then generate unit tests.",
         Refactor = "Refactor the code to improve clarity and readability.",
-        TextEnhance = "I'd like you to look at the text I wrote and edit it to make it sound more natural to a native English speaker. Do only minimal/minor edits without changing the text's tone.",
-        TextSummarize = "Please summarize the following text.",
+        TextEnhance = "\
+          I'd like you to look at the text I wrote and edit it to make it sound more natural to a native English speaker. \
+          Do only minimal/minor edits without changing the text's tone.",
+        TextSummarize = "\
+          You are an expert content summarizer. \
+          You take content in and output a Markdown formatted summary using the format below. \
+          Take a deep breath and think step by step about how to best accomplish this goal using the following steps. \
+          Combine all of your understanding of the content into a single, 20-word sentence in a section called ONE SENTENCE SUMMARY:. \
+          Output the 10 most important points of the content as a list with no more than 15 words per point into a section called MAIN POINTS:. \
+          Output a list of the 5 best takeaways from the content in a section called TAKEAWAYS:.",
+        TextSummarizeTwo = "Please summarize the following text.",
         TextSpelling = "Please correct any grammar and spelling errors in the following text.",
         TextWording = "Please improve the grammar and wording of the following text.",
         TextConcise = "Please rewrite the following text to make it more concise.",
+        TextMadrFeedback = "\
+          You are an expert in the Markdown Any Decision Records (MADR) format. \
+          You are my writing coach, helping make clear and concise MADRs. \
+          Slowly listen to the input given, and spend 4 hours of virtual time thinking about what they were probably thinking when they created the input.. \
+          In a section called RECOMMENDATIONS, give up to 5 bullet-points on providing a better MADR. \
+          In a section called EXAMPLES, take each recommendataion from the RECOMMENDATIONS sections, and provide up to 3 implementations of the recommendatios",
       },
     },
     build = function()
